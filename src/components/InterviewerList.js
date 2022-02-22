994f712cbb5c02a58af51b539382dd54363df05b
Eileen Li <eileenlimur@gmail.com>
2022-02-22 17:24:43 -0500

    Map props.interviewers to InterviewerListItems with props

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import InterviewerListItem from './InterviewerListItem';
 
 import './InterviewerList.scss';
 
 export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        id={interviewer.id}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={props.setInterviewer}
+        avatar={interviewer.avatar}
+        name={interviewer.name}
+      />
+    )
+  })
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {interviewers}
+      </ul>
     </section>
   )
 }
