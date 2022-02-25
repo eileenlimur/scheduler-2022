d1641b453f87ae93b29c65b4311f1631ed88e167
Eileen Li <eileenlimur@gmail.com>
2022-02-25 14:43:32 -0500

    Use state and props in Form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import Button from '../Button';
+import InterviewerList from '../InterviewerList';
 
 export default function(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -11,20 +15,20 @@ export default function(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            /*
-              This must be a controlled component
-              your code goes here
-            */
+            value={student}
+            onChange={e=>setStudent(e.target.value)}
           />
         </form>
         <InterviewerList 
-          /* your code goes here */
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger {/* your code goes here */}>Cancel</Button>
-          <Button confirm {/* your code goes here */}>Save</Button>
+          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
