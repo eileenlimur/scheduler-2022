e0de76f4638aaf4cd85b0e49ec81bb7486f598cd
Eileen Li <eileenlimur@gmail.com>
2022-03-15 23:36:02 -0400

    Add Appointment Empty, Booked stories

--- stories/index.js
+++ stories/index.js
@@ -193,3 +193,19 @@ storiesOf("Appointment", module)
       onCancel={action("onCancel")}
     />
   ))
+  .add("Appointment Empty", () => (
+    <>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </>
+  ))
+  .add("Appointment Booked", () => (
+    <>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </>
+  ))
