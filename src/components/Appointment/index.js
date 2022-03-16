4d2c068d7ddd9aaed5f7c24c3d74e96d9c45fb3d
Eileen Li <eileenlimur@gmail.com>
2022-03-15 23:35:49 -0400

    Add Header, Show, Empty components to Aptmt

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,21 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 import './styles.scss';
 
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      {!props.time && "No appointments"}
-      {props.time && `Appointment at ${props.time}`}
+      <Header time={props.time}/>
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        />
+      ) : (
+        <Empty />
+      )}
     </article>
   )
 }
